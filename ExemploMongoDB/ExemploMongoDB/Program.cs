b16d5e5abf6b290c7fc36a6184e23c9dd15e1487
Your Name <felipe.augustotm@gmail.com>
2016-07-18 23:05:39 -0300

    Feito exemplo de integração de mongo db .NET

---
+++ ExemploMongoDB/ExemploMongoDB/Program.cs
@@ -0,0 +1,74 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+
+using System;
+using System.Collections.Generic;
+
+namespace ExemploMongoDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string conexaoMongo = "mongodb://localhost/";
+                MongoClient client = new MongoClient(conexaoMongo);
+                var dataBase = client.GetDatabase("TesteMongoDB");
+
+                //GetPessoas(client, dataBase);
+                //InsertTeste(dataBase);
+                //GetTeste(dataBase);
+                //GetTeste2(dataBase);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        private static void GetTeste2(IMongoDatabase dataBase)
+        {
+            var teste = dataBase.GetCollection<Teste>("Teste");
+            var result = teste.Find(qr => qr.Namorada != null).ToList();
+        }
+
+        private static void GetTeste(IMongoDatabase dataBase)
+        {
+            var teste = dataBase.GetCollection<BsonDocument>("Teste");
+
+            var result = teste.Find(new BsonDocument()).ToList();
+        }
+
+        private static void InsertTeste(IMongoDatabase dataBase)
+        {
+            var teste = dataBase.GetCollection<BsonDocument>("Teste");
+
+            //1º forma de inserção
+            //var documento = new BsonDocument() {
+            //        {"Nome","Fulano"},
+            //        {"Cidade", "são Paulo"}
+            //    };
+
+
+            //2º Forma de inserção
+            var documento = new BsonDocument() {
+                    {"Nome","Felipe"},
+                    {"Namorada", new BsonDocument() {
+                        { "Nome", "Joice Marques"},
+                        { "Idade", 18}
+                    }
+                   }                    
+                };
+
+            teste.InsertOne(documento);
+        }        
+
+        private static List<Pessoa> GetPessoas(MongoClient clientm, IMongoDatabase dataBase)
+        {
+            var colecao = dataBase.GetCollection<Pessoa>(typeof(Pessoa).Name);
+            var r = colecao.Find(qr => qr.Idade == 21).ToList();
+            return r;
+        }
+    }
+}
---
+++ ExemploMongoDB/ExemploMongoDB/Teste.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploMongoDB
+{
+    public class Teste
+    {
+        public ObjectId _id { get; set; }
+        public string Nome { get; set; }
+        public Namorada Namorada { get; set; }
+    }
+
+    public class Namorada
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+    }
+}
